6e70db860b7bd902cd70adf0e0711fe2db70c4f5
Luiz Rodrigo de Souza <me@lurodrigo.com>
2020-07-28 19:51:19 -0300

    Bump after clojars deploy

--- project.clj
+++ project.clj
@@ -1,13 +1,11 @@
-(defproject lurodrigo/ratelimiter "0.1.2"
+(defproject lurodrigo/ratelimiter "0.1.3-SNAPSHOT"
   :description "Clojure wrapper for Resilience4j's Rate Limiter."
   :url "http:/github.com/lurodrigo/ratelimiter"
   :license {:name "MIT License"
             :url  "https://raw.githubusercontent.com/lurodrigo/ratelimiter/master/LICENSE"}
-  :deploy-repositories [["releases" :clojars]
-                        ["snapshots" :clojars]]
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.github.resilience4j/resilience4j-ratelimiter "1.5.0"]]
-  :profiles {:test [lambdaisland/kaocha "1.0.641"]}
+  :dependencies [[io.github.resilience4j/resilience4j-ratelimiter "1.5.0"]]
+  :profiles {:test {:repositories [[lambdaisland/kaocha "1.0.641"]]}
+             :dev  {:repositories [[org.clojure/clojure "1.10.1"]]}}
   :repl-options {:init-ns rate-limiter.core}
   :scm {:name "git"
         :url  "https://github.com/lurodrigo/ratelimiter"})
