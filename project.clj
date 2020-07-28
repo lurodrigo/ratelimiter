27d10c6893fea2fa93c44e23cd22eaf44547a2e1
Luiz Rodrigo de Souza <me@lurodrigo.com>
2020-07-28 19:29:34 -0300

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lurodrigo/ratelimiter "0.1.1-SNAPSHOT"
+(defproject lurodrigo/ratelimiter "0.1.1"
   :description "Clojure wrapper for Resilience4j's Rate Limiter."
   :url "http:/github.com/lurodrigo/ratelimiter"
   :license {:name "MIT License"
