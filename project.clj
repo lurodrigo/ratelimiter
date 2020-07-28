7aa07e707ce8966593255c8b0e7086774b72ec57
Luiz Rodrigo de Souza <me@lurodrigo.com>
2020-07-28 19:22:17 -0300

    bump, clojars creds

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject lurodrigo/ratelimiter "0.1.0"
+(defproject lurodrigo/ratelimiter "0.1.1-SNAPSHOT"
   :description "Clojure wrapper for Resilience4j's Rate Limiter."
   :url "http:/github.com/lurodrigo/ratelimiter"
   :license {:name "MIT License"
-            :url  "https://github.com/lurodrigo"}
+            :url  "https://raw.githubusercontent.com/lurodrigo/ratelimiter/master/LICENSE"}
+  :repositories [["clojars" {:url "https://clojars.org/repo/" :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.github.resilience4j/resilience4j-ratelimiter "1.5.0"]]
   :profiles {:test [lambdaisland/kaocha "1.0.641"]}
   :repl-options {:init-ns rate-limiter.core}
   :scm {:name "git"
-        :url  "https://source.developers.google.com/p/polvo-infra/r/gatherer"})
+        :url  "https://github.com/lurodrigo/ratelimiter"})
